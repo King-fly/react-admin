05d1228f1f6be174872df8065203a71c19cb635e
Emmanuel QUENTIN <manu.quentin@gmail.com>
2015-05-22 16:49:08 +0200

    Fix layout

--- app/View/Dashboard.js
+++ app/View/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 class DashboardView extends React.Component {
     render() {
         return (
-            <h1>DashboardView</h1>
+            <h1>Dashboard</h1>
         )
     }
 }
